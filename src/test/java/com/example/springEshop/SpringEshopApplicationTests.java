d666d8a43244ab9b786360d287efa5cba9ee8e08
eralash911 <unreal725@gmail.com>
2022-12-04 03:37:11 +0300

    Initial commit

---
+++ src/test/java/com/example/springEshop/SpringEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springEshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
